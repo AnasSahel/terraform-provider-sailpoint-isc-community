abe292254fabbbc5752228dfaef3e94922e8bf4c
Anas SAHEL <anas.sahel@gmail.com>
2025-09-16 10:08:03 +0000

    test: parameterize resource name in TestAccOrderResource for unique test cases

--- internal/provider/transform_resource_test.go
+++ internal/provider/transform_resource_test.go
@@ -5,32 +5,33 @@ package provider
 
 import (
 	"fmt"
+	"math/rand"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
 )
 
 func TestAccOrderResource(t *testing.T) {
+	randomSuffix := fmt.Sprintf("%d", rand.Int63())
 	resource.Test(t, resource.TestCase{
 		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
 		Steps: []resource.TestStep{
 			// Create and Read testing
 			{
-				Config: providerConfig + `
-				resource "sailpoint_transform" "test_acc" {
-					name = "test-acc"
+				Config: providerConfig + fmt.Sprintf(`
+				resource "sailpoint_transform" "test_acc_%s" {
+					name = "test-acc-%s"
 					type = "upper"
 					attributes = jsonencode({
 						value = "test-value"
 					})
 				}
-				`,
+				`, randomSuffix, randomSuffix),
 				Check: resource.ComposeAggregateTestCheckFunc(
-					resource.TestCheckResourceAttr("sailpoint_transform.test_acc", "name", "test-acc"),
-					resource.TestCheckResourceAttr("sailpoint_transform.test_acc", "type", "upper"),
-					resource.TestCheckResourceAttr("sailpoint_transform.test_acc", "type", "upper"),
-					resource.TestCheckResourceAttr("sailpoint_transform.test_acc", "internal", "false"),
-					resource.TestCheckResourceAttrWith("sailpoint_transform.test_acc", "attributes", func(s string) error {
+					resource.TestCheckResourceAttr(fmt.Sprintf("sailpoint_transform.test_acc_%s", randomSuffix), "name", fmt.Sprintf("test-acc-%s", randomSuffix)),
+					resource.TestCheckResourceAttr(fmt.Sprintf("sailpoint_transform.test_acc_%s", randomSuffix), "type", "upper"),
+					resource.TestCheckResourceAttr(fmt.Sprintf("sailpoint_transform.test_acc_%s", randomSuffix), "internal", "false"),
+					resource.TestCheckResourceAttrWith(fmt.Sprintf("sailpoint_transform.test_acc_%s", randomSuffix), "attributes", func(s string) error {
 						if s != `{"value":"test-value"}` {
 							return fmt.Errorf("expected attributes to be {\"value\":\"test-value\"}, got %s", s)
 						}
@@ -40,27 +41,26 @@ func TestAccOrderResource(t *testing.T) {
 			},
 			// ImportState testing
 			{
-				ResourceName:      "sailpoint_transform.test_acc",
+				ResourceName:      fmt.Sprintf("sailpoint_transform.test_acc_%s", randomSuffix),
 				ImportState:       true,
 				ImportStateVerify: true,
 			},
 			// Update and Read testing
 			{
-				Config: providerConfig + `
-				resource "sailpoint_transform" "test_acc" {
-					name = "test-acc"
+				Config: providerConfig + fmt.Sprintf(`
+				resource "sailpoint_transform" "test_acc_%s" {
+					name = "test-acc-%s"
 					type = "upper"
 					attributes = jsonencode({
 						value = "new-test-value"
 					})
 				}
-				`,
+				`, randomSuffix, randomSuffix),
 				Check: resource.ComposeAggregateTestCheckFunc(
-					resource.TestCheckResourceAttr("sailpoint_transform.test_acc", "name", "test-acc"),
-					resource.TestCheckResourceAttr("sailpoint_transform.test_acc", "type", "upper"),
-					resource.TestCheckResourceAttr("sailpoint_transform.test_acc", "type", "upper"),
-					resource.TestCheckResourceAttr("sailpoint_transform.test_acc", "internal", "false"),
-					resource.TestCheckResourceAttrWith("sailpoint_transform.test_acc", "attributes", func(s string) error {
+					resource.TestCheckResourceAttr(fmt.Sprintf("sailpoint_transform.test_acc_%s", randomSuffix), "name", fmt.Sprintf("test-acc-%s", randomSuffix)),
+					resource.TestCheckResourceAttr(fmt.Sprintf("sailpoint_transform.test_acc_%s", randomSuffix), "type", "upper"),
+					resource.TestCheckResourceAttr(fmt.Sprintf("sailpoint_transform.test_acc_%s", randomSuffix), "internal", "false"),
+					resource.TestCheckResourceAttrWith(fmt.Sprintf("sailpoint_transform.test_acc_%s", randomSuffix), "attributes", func(s string) error {
 						if s != `{"value":"new-test-value"}` {
 							return fmt.Errorf("expected attributes to be {\"value\":\"new-test-value\"}, got %s", s)
 						}
