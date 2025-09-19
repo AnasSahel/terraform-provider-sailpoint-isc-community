83f82ff739d39996c766d5ee99bbea452e06bd70
Anas SAHEL <anas.sahel@gmail.com>
2025-09-19 07:06:45 +0000

    refactor: reorganize provider_test.go for improved structure and clarity

--- internal/provider/provider_test.go
+++ internal/provider/provider_test.go
@@ -3,5 +3,22 @@
 
 package provider
 
+import (
+	"github.com/hashicorp/terraform-plugin-framework/providerserver"
+	"github.com/hashicorp/terraform-plugin-go/tfprotov6"
+)
+
 // This file is kept for future provider-level tests.
 // Individual resource and data source tests are located in their respective service packages.
+
+const (
+	providerConfig = `
+		provider "sailpoint" {}
+	`
+)
+
+var (
+	testAccProtoV6ProviderFactories = map[string]func() (tfprotov6.ProviderServer, error){
+		"sailpoint": providerserver.NewProtocol6WithError(New("test")()),
+	}
+)
